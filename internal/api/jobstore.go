348f2d7306119c20759f43f7e4d1b4ca363062b5
ashish111333 <ashishkumar0265aaa@gmail.com>
2026-07-11 12:59:34 +0530

    update jobStore
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- internal/api/jobstore.go
+++ internal/api/jobstore.go
@@ -3,7 +3,8 @@ package api
 // job store represents the DB/store where jobs will be stored
 
 type JobStore interface {
-	saveJob() error
+	saveJob(*Job) error
 	getJob() (*Job, error)
 	deleteJob() error
+	getJobsByTypeAndStatus(string, string) error
 }
