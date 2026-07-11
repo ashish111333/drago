05517c3c871d5d901250ec691d8736ec19a71051
ashish111333 <ashishkumar0265aaa@gmail.com>
2026-07-12 00:56:59 +0530

    update jobstore
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- internal/api/jobstore.go
+++ internal/api/jobstore.go
@@ -1,10 +1,12 @@
 package api
 
+import "github.com/google/uuid"
+
 // job store represents the DB/store where jobs will be stored
 
 type JobStore interface {
 	saveJob(*Job) error
 	getJob() (*Job, error)
-	deleteJob() error
-	getJobsByTypeAndStatus(string, string) error
+	deleteJob(id uuid.UUID) error
+	getJobsByTypeAndStatus(type_ string, status_ string) error
 }
