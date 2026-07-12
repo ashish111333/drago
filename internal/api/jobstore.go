b74778806b9a5295685e1bde843d3a63384e517e
ashish111333 <ashishkumar0265aaa@gmail.com>
2026-07-13 05:27:55 +0530

    update jobstore
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- internal/api/jobstore.go
+++ internal/api/jobstore.go
@@ -1,14 +1,45 @@
 package api
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/google/uuid"
+)
 
 // job store interface
 
 type JobStore interface {
-	saveJob(*Job) error
-	getJob() (*Job, error)
-	deleteJob(id uuid.UUID) error
-	getJobsByTypeAndStatus(type_ string, status_ string) error
-	deleteJobsByStatus(string) error
-	deleteJobsByStatusAndType(type_, status_ string) error
+	SaveJob(context.Context, *Job) error
+	GetJob(context.Context) (*Job, error)
+	DeleteJob(ctx context.Context, id uuid.UUID) error
+	GetJobsByTypeAndStatus(ctx context.Context, type_ string, status_ string) error
+	DeleteJobsByStatus(context.Context, string) error
+	DeleteJobsByStatusAndType(ctx context.Context, type_, status_ string) error
 }
+
+type PgJobStore struct{}
+
+func (pgs *PgJobStore) Connect() error {
+
+	conn_url := os.Getenv("PG_CONN_URL")
+	if conn_url == "" {
+		return errors.New("connection url is empty: can't connect to datastore")
+	}
+	return nil
+
+}
+
+func (pgs *PgJobStore) SaveJob() {
+
+}
+func (pgs *PgJobStore) GetJob() {}
+
+func (pgs *PgJobStore) DeleteJob() {
+
+}
+
+func (pgs *PgJobStore) GetJobsByTypeAndStatus()    {}
+func (pgs *PgJobStore) DeleteJobsByStatusAndType() {}
+func (pgs *PgJobStore) DeleteJobsByStatus()        {}
